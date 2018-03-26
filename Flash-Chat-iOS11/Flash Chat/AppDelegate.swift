c339563b0f8d9d3847dc3008e31917a45d1ff65f
Sean S. Kim <kssc0112@Seans-MacBook-Pro.local>
2018-03-26 12:59:45 -0700

    fetches data from firebase

--- Flash-Chat-iOS11/Flash Chat/AppDelegate.swift
+++ Flash-Chat-iOS11/Flash Chat/AppDelegate.swift
@@ -20,8 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
-        let myDb = Database.database().reference()
-        myDb.setValue("hello world")
         return true
     }
 
